cb549ad7c302c6e2fa5bde12995620d2a1a273fb
Steven Esser <steven.michael.esser@gmail.com>
2014-07-21 13:41:55 -0700

    LAWL error check didnt work

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,4 +1,27 @@
 class ProblemsController < ApplicationController
+  def index
+    @problems = Problem.all
+  end
+
+  def show 
+    @problem = Problem.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    @problem = Problem.new(problem_params)
+    
+    if @problem.save
+      redirect_to @problem
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def problem_params
+      params.require(:problem).permit(:name, :description)
+    end
 end
---
+++ app/models/problem.rb
@@ -0,0 +1,4 @@
+class Problem < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { minimum: 3 }
+end
